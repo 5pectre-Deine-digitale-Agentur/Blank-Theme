744b4840572ced61dd882c2f9191f06b59954ee2
Erik Jung <erik.jung@5pectre.com>
2021-03-07 16:11:40 +0100

    new favicon

--- header.php
+++ header.php
@@ -14,8 +14,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicons/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicons/apple-touch-icon.png" rel="apple-touch-icon-precomposed">
 
 		<?php wp_head(); ?>
 
