abbf5214f449c85ea250aebdfb7c1757b589ef71
derEinzigWahreSchnabel <christian.schnabel@5pectre.com>
2021-03-09 22:04:34 +0100

    Mobilseite 5pectre bearbeitet

--- sections/navigation.php
+++ sections/navigation.php
@@ -2,16 +2,16 @@
 <section id="navigation">
     <div class="container">
       <div class="row">
-        <div class="col-xxl-4 col-lg-4 col-md-0 col-sm-0 col-xs-0 menu-item-col ninja">
+        <div class="col-xxl-0 col-lg-0 col-md-10 col-sm-5 col-xs-5 menu-item-col ">
           <?php wp_nav_menu(array( 'theme_location' => 'main' )); ?>
         </div>
-        <div class="col-xxl-4 col-lg-4 col-md-6 col-sm-3 col-xs-6 nav-img-col">
-          <a href="<?php echo get_home_url();?>">
-            <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logos/logo.png" alt="">
-          </a>
-        </div>
-        <div class="col-xxl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6  nav-icon-col">
-          <?php wp_nav_menu(array( 'theme_location' => 'extra' )); ?>
+        <div class="number col-xxl-0 col-lg-0 col-md-2 col-sm-5 col-xs-5">
+          <ul>
+            <li>01</li>
+            <li>02</li>
+            <li>03</li>
+            <li>04</li>
+          </ul>
         </div>
       </div>
     </div>
