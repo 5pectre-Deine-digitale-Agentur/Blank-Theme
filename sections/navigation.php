3e7e46d727103de1c2c371b3377907a38257ef00
derEinzigWahreSchnabel <christian.schnabel@5pectre.com>
2021-03-17 19:36:17 +0100

    Zwischenstand

--- sections/navigation.php
+++ sections/navigation.php
@@ -2,17 +2,37 @@
 <section id="navigation">
     <div class="container">
       <div class="row">
-        <div class="col-xxl-0 col-lg-0 col-md-10 col-sm-5 col-xs-5 menu-item-col ">
+        <div class="col-xxl-0 col-lg-0 col-md-6 col-sm-6 col-xs-6 menu-item-col ">
           <?php wp_nav_menu(array( 'theme_location' => 'main' )); ?>
         </div>
-        <div class="number col-xxl-0 col-lg-0 col-md-2 col-sm-5 col-xs-5">
+        <!-- <div class="number col-xxl-0 col-lg-0 col-md-2 col-sm-2 col-xs-2">
           <ul>
             <li>01</li>
             <li>02</li>
             <li>03</li>
             <li>04</li>
           </ul>
-        </div>
+        </div> -->
       </div>
     </div>
+</section>
+
+<section id="head" class="head">
+  <div class="text">
+    <h1>Einfach eine weitere Agentur</h1>
+    <h2>Nur besser</h2>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xxl-0 col-lg-0 col-md-3 col-sm-3 col-xs-3 button">
+        <img src="" alt="">
+      </div>
+      <div class="col-xxl-0 col-lg-0 col-md-3 col-sm-3 col-xs-3 button">
+        <img src="" alt="">
+      </div>
+      <div class="col-xxl-0 col-lg-0 col-md-3 col-sm-3 col-xs-3 button">
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
 </section>
