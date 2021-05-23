d27180949ee4b6f7db9ca1787869f1d62027ef26
Erik Jung <erik.jung@5pectre.com>
2021-05-23 19:18:42 +0200

    added Hero 2 to Templates

---
+++ hero.php
@@ -0,0 +1,14 @@
+<?php
+/* 		Template Name: Hero Sections		*/
+get_header(); ?>
+
+	<main role="main">
+		<?php
+			include 'sections/Hero/hero-2.php';
+			include 'sections/Hero/hero-3.php';
+		?>
+	</main>
+
+<?php //get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ sections/Hero/hero-3.php
@@ -0,0 +1,23 @@
+<section id="hero" class="hero-2">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+          <div class="content-container">
+            <div class="content">
+
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+
+      </div>
+      <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+
+      </div>
+    </div>
+  </div>
+</section>
