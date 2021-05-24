722fa1701d1b79b5cb0797dfe10aa15b0ba7d35a
Erik Jung <erik.jung@5pectre.com>
2021-05-24 09:40:21 +0200

    added Hero-9

--- hero.php
+++ hero.php
@@ -7,6 +7,7 @@ get_header(); ?>
 			include 'sections/Hero/hero-2.php';
 			include 'sections/Hero/hero-3.php';
 			include 'sections/Hero/hero-8.php';
+			include 'sections/Hero/hero-9.php';
 		?>
 	</main>
 
---
+++ sections/Hero/hero-9.php
@@ -0,0 +1,38 @@
+<section id="hero" class="hero-9">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="container upper" style="background: url('https://images.pexels.com/photos/3144218/pexels-photo-3144218.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'); background-size: cover; background-position: center;">
+        <div class="row justify-content-center">
+          <div class="col col-xl-8 col-lg-10">
+            <div class="content-container">
+              <div class="content">
+                <h1 class="pagetitle light-text">Big Title</h1>
+                <span class="subtitle light-text">Section Title</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container lower">
+        <div class="row justify-content-center">
+          <div class="col col-xl-4 col-lg-5 col-md-6 col-sm-6 col-xs-12">
+            <div class="content-container">
+              <div class="content">
+                <h4>Lorem Ipsum</h4>
+                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</span>
+              </div>
+            </div>
+          </div>
+          <div class="col col-xl-4 col-lg-5 col-md-6 col-sm-6 col-xs-12">
+            <div class="content-container">
+              <div class="content">
+                <h4>Lorem Ipsum</h4>
+                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
