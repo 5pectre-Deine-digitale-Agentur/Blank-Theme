21494af0491a27eb533c644267f1f8b0c9c2b2a8
Erik Jung <erik.jung@5pectre.com>
2021-10-23 18:49:47 +0200

    added ACF Ecxport files

--- Theme/sections/01_ui/pagebuild.php
+++ Theme/sections/01_ui/pagebuild.php
@@ -2,6 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
+        
         <?php if( have_rows('content') ):
             while ( have_rows('content') ) : the_row();
 
